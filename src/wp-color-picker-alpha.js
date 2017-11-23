f08f4bc1538bfb48b7bb17b3183fc21379233754
Justin Sternberg <me@jtsternberg.com>
2017-11-22 21:37:45 -0500

    Bump version.

--- src/wp-color-picker-alpha.js
+++ src/wp-color-picker-alpha.js
@@ -4,7 +4,7 @@
  * Overwrite Automattic Iris for enabled Alpha Channel in wpColorPicker
  * Only run in input and is defined data alpha in true
  *
- * Version: 2.1.2
+ * Version: 2.1.3
  * https://github.com/kallookoo/wp-color-picker-alpha
  * Licensed under the GPLv2 license.
  */
